b085027372c3629afd8dde106c5c3b46956ead94
박정호 <jungho.park@kurlycorp.com>
2025-03-02 16:47:53 +0900

    다음음악 재생 유즈케이스 구현

--- domain/src/main/java/com/zs/jyoon/domain/usecase/PlayNextMedia.kt
+++ domain/src/main/java/com/zs/jyoon/domain/usecase/PlayNextMedia.kt
@@ -2,14 +2,45 @@ package com.zs.jyoon.domain.usecase
 
 import com.zs.jyoon.domain.core.BaseUseCase
 import com.zs.jyoon.domain.core.Logger
+import com.zs.jyoon.domain.core.player.type.RepeatType
+import com.zs.jyoon.domain.repositoy.PlaybackRepository
 import javax.inject.Inject
 
 class PlayNextMedia @Inject constructor(
-    logger: Logger
-) : BaseUseCase<Unit, Unit>(
+    logger: Logger,
+    private val playbackRepository: PlaybackRepository
+) : BaseUseCase<Unit, Boolean>(
     logger
 ) {
-    override suspend fun execute(param: Unit): Unit {
-        TODO("Not yet implemented")
+    override suspend fun execute(param: Unit): Boolean {
+        val currentPlayingList = playbackRepository.currentPlayingList.value
+        val currentPlayingItem = playbackRepository.currentPlayingItem.value
+        val currentIndex = currentPlayingList.indexOf(currentPlayingItem)
+
+        val repeatType = playbackRepository.currentRepeatType.value
+
+        val isLastItem = currentIndex == currentPlayingList.size - 1
+
+        return when (repeatType) {
+            RepeatType.NONE -> {
+                if (isLastItem) {
+                    false
+                } else {
+                    playbackRepository.setSeekPosition(0)
+                    true
+                }
+            }
+
+            RepeatType.ONE -> {
+                playbackRepository.setSeekPosition(0)
+                true
+            }
+
+            RepeatType.ALL -> {
+                playbackRepository.setCurrentPlayingItem(currentPlayingList.first())
+                playbackRepository.setSeekPosition(0)
+                true
+            }
+        }
     }
 }
